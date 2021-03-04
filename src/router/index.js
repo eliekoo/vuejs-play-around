7bdeefe934aec502d0d27d1590ec8e10cd68f47d
elie_koo <elie@easystore.co>
2021-03-04 20:17:26 +0800

    Update file

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/rain',                    name: 'rain',                       component: require('./../components/RainEffect.vue').default },
+  { path: '/snow',                    name: 'snow',                       component: require('./../components/SnowEffect.vue').default },
+  { path: '/test',                    name: 'test',                       component: require('./../components/HelloWorld.vue').default },
+  
+]
+
+// eslint-disable-next-line no-new
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+// const app = new Vue({
+//   router
+// }).$mount('#app')
+
+export default router
